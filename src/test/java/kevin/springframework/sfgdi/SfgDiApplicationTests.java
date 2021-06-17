8b461f81bc3ea7aed567cce7291430eda6e14703
kevinanm <kevin.moreno@openmarket.com>
2021-06-17 15:38:03 -0500

    spring boot initialzr

---
+++ src/test/java/kevin/springframework/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package kevin.springframework.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
