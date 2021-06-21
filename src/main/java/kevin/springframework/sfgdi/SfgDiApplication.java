3efe82e9b72a3ae05d47029f421f0c111d7eb8e4
kevinanm <kevin.moreno@openmarket.com>
2021-06-21 17:45:09 -0500

    Factory Beans

--- src/main/java/kevin/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/kevin/springframework/sfgdi/SfgDiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"kevin.springframework.sfgdi","com.springframework.pets"})
+//@ComponentScan(basePackages = {"kevin.springframework.sfgdi","com.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
--- src/main/java/kevin/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/kevin/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,5 +1,7 @@
 package kevin.springframework.sfgdi.config;
 
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
 import kevin.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import kevin.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import kevin.springframework.sfgdi.services.*;
@@ -10,6 +12,23 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+       return  petServiceFactory.getPetService("dog");
+    }
+
+    @Profile({"cat"})
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return  petServiceFactory.getPetService("cat");
+    }
+
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
+    }
 
     @Profile({"ES", "default"})
     @Bean("i18nService")
